2bc2f1cd7d771f3030b60cb3466e0fa25900b58f
Victor Diaz de Leon <vic.abdiel@gmail.com>
2021-02-22 17:28:30 -0600

    Update Constants.groovy

--- src/org/example/Constants.groovy
+++ src/org/example/Constants.groovy
@@ -1,4 +1,6 @@
-package org.exampleclass Constants {
+package org.example
+
+class Constants {
   static final String SLACK_MESSAGE = "Sending Slack Notification"
   static final String EMAIL_MESSAGE = "Sending Email"
 }
